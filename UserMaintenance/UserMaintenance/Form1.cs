52c2ddbacd5a0ec5496660c0c364e52d303bd3b5
Balázs Tibor Gulybán <balazs.gulyban@stud.uni-corvinus.hu>
2022-09-30 13:05:54 +0200

    9. task

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             lblLastName.Text = Resource1.FullName;
             btnAdd.Text = Resource1.Add;
+            btnWrite.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -34,6 +36,21 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+            textBox1.Text = "";
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = ".csv";
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+            StreamWriter sw = new StreamWriter(sfd.FileName);
+            foreach (var item in users)
+            {
+                sw.WriteLine(item.ID + ";" + item.FullName);
+            }
+            sw.Close();
         }
     }
 }
