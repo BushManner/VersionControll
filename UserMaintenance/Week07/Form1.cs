c1ad703379d67d284c8d694f211b93edfbd5dd85
Balázs Tibor Gulybán <balazs.gulyban@stud.uni-corvinus.hu>
2022-11-11 12:26:40 +0100

    Portfólió érték kiszámítása

--- UserMaintenance/Week07/Form1.cs
+++ UserMaintenance/Week07/Form1.cs
@@ -23,6 +23,7 @@ namespace Week07
             dataGridView1.DataSource = Ticks;
 
             CreatePortfolio();
+            
         }
 
         private void CreatePortfolio()
@@ -32,6 +33,21 @@ namespace Week07
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
